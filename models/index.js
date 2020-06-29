ab16b2e51f67b3694edd7844c1b7f4f30462fd59
Louis <lreillywillhight@gmail.com>
2020-06-29 14:54:36 -0700

    views/auth/login and views/auth register

--- models/index.js
+++ models/index.js
@@ -1,1 +1,8 @@
-// middleware decs
+// handle DB connection/middleware for apps
+'use strict'
+
+// import filesystem
+// import path
+// initialize ORM layer
+// import env variables
+// init DB variable
